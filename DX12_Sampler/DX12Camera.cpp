3a843b4cf4ac4d6f384e5ea5bbbf35042df928d7
Axel DESHORS <axel.deshors@hotmail.fr>
2018-01-18 12:15:22 +0100

    Fix the issue with aspect ratio of the projection matrix

--- DX12_Sampler/DX12Camera.cpp
+++ DX12_Sampler/DX12Camera.cpp
@@ -26,7 +26,7 @@ DX12Camera::DX12Camera()
 	// build projection matrix
 	DX12Window & wnd = DX12RenderEngine::GetInstance().GetWindow();
 
-	const float windowRatio = (float)(wnd.GetWidth() / wnd.GetHeight());
+	const float windowRatio = (float)((float)wnd.GetWidth() / (float)wnd.GetHeight());
 	XMMATRIX tmpProj = XMMatrixPerspectiveFovLH(90.f, windowRatio, 0.05f, 1000.f);
 
 	XMStoreFloat4x4(&m_Projection, tmpProj);
